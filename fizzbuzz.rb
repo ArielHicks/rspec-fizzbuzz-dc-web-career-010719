1594aeec50cffa816bca640027fcb623e5eda2d9
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-22 18:29:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@
       "Fizz" # Go Fizz
     elsif int % 5 == 0 #if the number int is divisible by 5
       "Buzz" # Go Buzz
-    else int % 3 == 0 && int % 5 == 0
-      "FizzBuzz"
+    else int % 3 == 0 && int % 5 == 0 #if the number int is divisible by 3 and 5
+      "FizzBuzz" #Go FizzBuzz
   end
 end
